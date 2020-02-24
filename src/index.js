20c94601669547e9a14f106319a221added1ce04
Nikitas Nikolaev <troll-men@mail.ru>
2020-02-24 22:33:16 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array))
+    return 0;
+
+  array.sort((a,b) => a - b);  
+  return array.length > 0 ? array[0] : 0;  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array))
+    return 0;
+
+  array.sort((a,b) => b - a);
+  return array.length > 0 ? array[0] : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {  
+  let sum = 0;
+
+  if (!Array.isArray(array))
+    return 0;
+
+  array.forEach((item, index, array) => sum += item);
+  return array.length > 0 ? sum / array.length : 0;
 }
